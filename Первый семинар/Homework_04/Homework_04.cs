35a5ade3b409facb695236c89fd5fdc3e84634b7
Ksenaty1 <yarlay.learning@gmail.com>
2022-09-21 13:14:18 +0300

    added Homework_04

---
+++ Первый семинар/Homework_04/Homework_04.cs
@@ -0,0 +1,11 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.Write("Введите число N =>");
+int n = int.Parse(Console.ReadLine());
+int count = 2;
+while (count <= n){
+    if (count % 2 == 0) Console.Write($"{count}, ");
+    count ++;
+}
