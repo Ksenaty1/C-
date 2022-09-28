6798c45ff9025b8c204f75e6d1239fb755528541
Ksenaty1 <yarlay.learning@gmail.com>
2022-09-28 21:09:32 +0300

    added Homeworkd for lesson 3

---
+++ семинар_3/Homework_01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+
+int GetNumber()
+{
+    return int.Parse(Console.ReadLine());
+}
+
+
+int number = GetNumber();
+if (number / 10000 == number % 10 && (number / 1000) % 10 == (number / 10) % 10) Console.WriteLine("палиндром");
+else Console.WriteLine("Не палиндром");
---
+++ семинар_3/Homework_02/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+//Задаем координаты точки A
+Console.Write("Введиме координату X точки A: ");
+int coord_a_x = int.Parse(Console.ReadLine());
+Console.Write("Введиме координату Y точки A: ");
+int coord_a_y = int.Parse(Console.ReadLine());
+Console.Write("Введиме координату Z точки A: ");
+int coord_a_z = int.Parse(Console.ReadLine());
+
+
+//Задаем координаты точки B
+Console.Write("Введиме координату X точки B: ");
+int coord_b_x = int.Parse(Console.ReadLine());
+Console.Write("Введиме координату Y точки B: ");
+int coord_b_y = int.Parse(Console.ReadLine());
+Console.Write("Введиме координату Z точки B: ");
+int coord_b_z = int.Parse(Console.ReadLine());
+
+// Вычисляем расстояние
+double distance = Math.Sqrt(Math.Pow(coord_a_x - coord_b_x, 2) + Math.Pow(coord_a_y - coord_b_y, 2) + Math.Pow(coord_a_z - coord_b_z, 2));
+Console.WriteLine($"Расстояние: {distance}");
