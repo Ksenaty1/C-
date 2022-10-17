9d49fb1c7090a2d7dbae3f08c1897b4492a91d11
Ksenaty1 <yarlay.learning@gmail.com>
2022-10-17 19:36:38 +0300

    added Homework for lesson 8

---
+++ семинар_8/Homework_01/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+
+int[,] RandomMatrix()
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[3, 4];
+    for(int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] SortMatrix(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    temp =  array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+
+int[,] array = RandomMatrix();
+ShowMatrix(array);
+Console.WriteLine("--------------------------------------");
+ShowMatrix(SortMatrix(array));
---
+++ семинар_8/Homework_02/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int[,] RandomMatrix()
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[4, 4];
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindLowestLine(int[,] matrix)
+{
+    int lowest = 100000000;
+    int lowest_line = 0;
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        if (sum < lowest)
+        {
+            lowest = sum;
+            lowest_line = i;
+        }
+        sum = 0;
+    }
+    return lowest_line + 1;
+}
+
+int[,] array = RandomMatrix();
+ShowMatrix(array);
+Console.WriteLine("------------------------------");
+Console.WriteLine(FindLowestLine(array));
